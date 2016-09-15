35cb8cb294b02709e4720b57cf6d393252fd6084
Oliver Hoff <oliver@hofff.com>
2016-09-16 01:17:19 +0200

    remove FE modules and elements from contao config to make them unavailable

--- contao-module/config/config.php
+++ contao-module/config/config.php
@@ -1,16 +1,1 @@
 <?php
-
-$GLOBALS['TL_CTE']['includes']['bbit_mod_multiArt']
-	= 'ContentIncludeMultiArticles';
-
-$GLOBALS['TL_CTE']['includes']['bbit_mod_pageArt']
-	= 'ContentIncludePageArticles';
-
-$GLOBALS['FE_MOD']['miscellaneous']['bbit_mod_art']
-	= 'ModuleIncludeArticle';
-
-$GLOBALS['FE_MOD']['miscellaneous']['bbit_mod_multiArt']
-	= 'ModuleIncludeMultiArticles';
-
-$GLOBALS['FE_MOD']['miscellaneous']['bbit_mod_pageArt']
-	= 'ModuleIncludePageArticles';
